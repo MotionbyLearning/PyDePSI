2a2d98da30be5e7147a682d6c67e05f58055e20c
Ou Ku <o.ku@esciencecenter.nl>
2024-09-12 15:00:40 +0200

    add slc reconstruction

--- pydepsi/slc.py
+++ pydepsi/slc.py
@@ -1,13 +1,39 @@
 """slc.py: Functions for SLC related operations."""
 
+import dask.array as da
+import numpy as np
 
-# def intf_to_slc(mother_slc, ifg, blockwise=False):
-#     return (ifg * mother_slc.conj())/(np.abs(mother_slc)**2)
 
-# def intf_to_slc_stack(mother_slc, ifgs, blockwise=False):
-#     len(ifgs.time)
+def intf_to_slc(mother_slc, ifgs):
+    """Convert a stack of interferograms to SLCs.
 
-#     slc_prime_all = []
-#     for ifg in ifgs.time:
-#         slc_prime_all.append(intf_to_slc(mother_slc, ifgs[:,:,ifg]))
-#     return slc_prime_all
+    The conversion will be implemented by conjugated multiplication of the interferograms complex values
+    with the complex values of the mother SLC, and then dividing by the squared magnitude of the mother complex.
+
+    Parameters
+    ----------
+    mother_slc : Xarray.Dataset
+        Mother SLC. This Dataset should have three dimensions ('azimuth', 'range', 'time').
+        The 'azimuth' and 'range' dimensions should be the same as `ifgs`.
+        The 'time' dimension should have size 1.
+    ifgs : Xarray.Dataset
+        Interferograms. This Dataset should have three dimensions ('azimuth', 'range', 'time').
+        The 'azimuth' and 'range' dimensions should be the same as `mother_slc`.
+
+    Returns
+    -------
+    Xarray.Dataset
+        SLCS converted from the interferograms.
+    """
+    slc_out = ifgs.copy()
+    meta_arr = np.array((), dtype=np.complex64)
+    slc_complex = da.apply_gufunc(
+        _slc_complex_recontruct, "(),()->()", mother_slc["complex"], slc_out["complex"], meta=meta_arr
+    )
+    slc_out = slc_out.assign({"complex": (("azimuth", "range", "time"), slc_complex)})
+    return slc_out
+
+
+def _slc_complex_recontruct(mother_slc_complex, ifg_complex):
+    """Reconstruct the SLC complex values from the mother SLC complex and the interferogram complex."""
+    return (ifg_complex * mother_slc_complex.conj()) / (np.abs(mother_slc_complex) ** 2)
